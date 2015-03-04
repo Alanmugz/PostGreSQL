b3f91807b7b11d521f749a7c73e9e14e672fe5bf
Alanmugz <Alanmugz@gmail.com>
2015-03-04 15:58:43 +0000

    Updated Queries

--- Code snippets.sql
+++ Code snippets.sql
@@ -31,24 +31,31 @@ SELECT x.id, '{
 FROM generate_series(1,1) AS x(id);
 
 INSERT INTO test (id, json)
-SELECT 2, '{
-	       "_id": "54f5ca1067fd4211c7211d45",
-	       "index": 2,
-	       "friends": [
-		       {
-			"id": 0,
-			"name": "Watts Morrison"
-		       },
-		       {
-			"id": 1,
-			"name": "Marcella Wilder"
-		       },
-		       {
-			"id": 2,
-			"name": "Terry Willis"
-		       }
-	          ]
-	       }'::jsonb
+SELECT 7, '{
+			"lookup_id": "dd20a605-5462-4ee2-86f6-eeae74931204",
+			"service_type": "ABC",
+			"metadata": "sampledata2",
+			"matrix": [
+					{
+						"payment_selection": "unselected",
+						"offer_currencies": [
+							{
+								"currency_name": "Euro",
+								"value": 123.45
+							}
+						]
+					},
+					{
+						"payment_selection": "selected",
+						"offer_currencies": [
+							{
+								"currency_name": "Euro",
+								"value": 123.45
+							}
+						]
+					}
+				]
+			}'::jsonb
 
 select * from test where json ? 'friends' -> 2;
 
@@ -63,6 +70,40 @@ SELECT * FROM test WHERE json ->> 'index' > '1';
 SELECT * FROM test WHERE json -> 'index' = '0';
 
 SELECT * FROM test WHERE json -> 'friends' -> 'id' = '0';
+
+
+select element ->> 'id'
+from test t, jsonb_array_elements(t.json -> 'friends') AS element
+where element ->> 'name' = 'Alan Mulligan';
+
+select json -> 'friends'
+from test t, jsonb_array_elements(t.json -> 'friends') AS element
+where element ->> 'name' = 'Alan Mulligan';
+
+
+
+SELECT element -> 'offer_currencies' -> 0 -> 'value'
+FROM   test t, jsonb_array_elements(t.json -> 'matrix') AS element
+WHERE  element ->> 'payment_selection' = 'mc'
+
+SELECT element -> 'offer_currencies' -> 0
+FROM   test t, jsonb_array_elements(t.json -> 'matrix') AS element
+WHERE  element ->> 'payment_selection' = 'mc'
+
+SELECT element -> 'offer_currencies'
+FROM   test t, jsonb_array_elements(t.json -> 'matrix') AS element
+WHERE  element ->> 'payment_selection' = 'mc'
+
+SELECT element
+FROM   test t, jsonb_array_elements(t.json -> 'matrix') AS element
+WHERE  element ->> 'payment_selection' = 'mc'
+
+SELECT first_currency.*
+FROM   test t,
+       jsonb_array_elements(t.json -> 'matrix') element,
+       jsonb_each_text(element -> 'offer_currencies' -> 0) AS first_currency
+WHERE  element ->> 'payment_selection' = 'mc'
+ 
 
 select '{"a":"abc","d":"def","z":[1,2,3],"d":"overwritten"}'::jsonb -> 'z' -> 2;
 
