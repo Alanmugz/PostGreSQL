746f7d0d1f3665f9235cc5374d5ca25bfa7bb053
Alanmugz <Alanmugz@gmail.com>
2015-03-05 16:26:52 +0000

    update to queries

--- Code snippets.sql
+++ Code snippets.sql
@@ -10,52 +10,140 @@ CREATE TABLE test
 )
 
 INSERT INTO test (id, json)
-SELECT x.id, '{
-	       "_id": "54f5ca1067fd4211c7211d45",
-	       "index": 0,
-	       "friends": [
-		       {
-			"id": 0,
-			"name": "Watts Morrison"
-		       },
-		       {
-			"id": 1,
-			"name": "Marcella Wilder"
-		       },
-		       {
-			"id": 2,
-			"name": "Terry Willis"
-		       }
-	          ]
-	       }'::jsonb
-FROM generate_series(1,1) AS x(id);
+11, '{
+					"lookup_id": "c30055e5-97f8-4aa4-99d1-8522039eab83",
+					"service_type": "MCM",
+					"metadata": "sampledata2",
+					"matrix": [
+						{
+							"payment_selection": "unselected",
+							"offer_currencies": [
+								{
+									"display_order": 1,
+									"currency_code": "EUR",
+									"currency_name": "Euro",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 1,
+									"offer_exchange_rate": 1,
+									"margin_percentage": 0,
+									"value": 1446.52,
+									"margin_value": 0
+								},
+								{
+									"display_order": 2,
+									"currency_code": "AUD",
+									"currency_name": "Australian Dollar",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 1.4612345,
+									"offer_exchange_rate": 1.4612345,
+									"margin_percentage": 0,
+									"value": 496.84,
+									"margin_value": 0
+								},
+								{
+									"display_order": 3,
+									"currency_code": "GBP",
+									"currency_name": "Pound Sterling",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 0.7512345,
+									"offer_exchange_rate": 0.7512345,
+									"margin_percentage": 0,
+									"value": 2316.19,
+									"margin_value": 0
+								},
+								{
+									"display_order": 4,
+									"currency_code": "USD",
+									"currency_name": "US Dollar",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 1.1312345,
+									"offer_exchange_rate": 1.1312345,
+									"margin_percentage": 0,
+									"value": 2865.15,
+									"margin_value": 0
+								}
+							]
+						},
+						{
+							"payment_selection": "mc",
+							"offer_currencies": [
+								{
+									"display_order": 1,
+									"currency_code": "EUR",
+									"currency_name": "Euro",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 1,
+									"offer_exchange_rate": 1,
+									"margin_percentage": 0,
+									"value": 1976.99,
+									"margin_value": 0
+								},
+								{
+									"display_order": 2,
+									"currency_code": "AUD",
+									"currency_name": "Australian Dollar",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 1.4612345,
+									"offer_exchange_rate": 1.5196839,
+									"margin_percentage": 4,
+									"value": 2454.66,
+									"margin_value": 7.5
+								},
+								{
+									"display_order": 3,
+									"currency_code": "USD",
+									"currency_name": "US Dollar",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 1.1312345,
+									"offer_exchange_rate": 1.1764839,
+									"margin_percentage": 4,
+									"value": 2618.87,
+									"margin_value": 5.81
+								}
+							]
+						},
+						{
+							"payment_selection": "vb",
+							"offer_currencies": [
+								{
+									"display_order": 1,
+									"currency_code": "EUR",
+									"currency_name": "Euro",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 1,
+									"offer_exchange_rate": 1,
+									"margin_percentage": 0,
+									"value": 3998.54,
+									"margin_value": 0
+								},
+								{
+									"display_order": 2,
+									"currency_code": "GBP",
+									"currency_name": "Pound Sterling",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 0.7512345,
+									"offer_exchange_rate": 0.7737715,
+									"margin_percentage": 3,
+									"value": 1269.17,
+									"margin_value": 2.87
+								},
+								{
+									"display_order": 3,
+									"currency_code": "USD",
+									"currency_name": "US Dollar",
+									"currency_exponent": 2,
+									"wholesale_exchange_rate": 1.1312345,
+									"offer_exchange_rate": 1.1651715,
+									"margin_percentage": 3,
+									"value": 892.85,
+									"margin_value": 4.32
+								}
+							]
+						}
+					]
+				}'::jsonb
+FROM generate_series(1,1000) AS x(id);
 
-INSERT INTO test (id, json)
-SELECT 7, '{
-			"lookup_id": "dd20a605-5462-4ee2-86f6-eeae74931204",
-			"service_type": "ABC",
-			"metadata": "sampledata2",
-			"matrix": [
-					{
-						"payment_selection": "unselected",
-						"offer_currencies": [
-							{
-								"currency_name": "Euro",
-								"value": 123.45
-							}
-						]
-					},
-					{
-						"payment_selection": "selected",
-						"offer_currencies": [
-							{
-								"currency_name": "Euro",
-								"value": 123.45
-							}
-						]
-					}
-				]
-			}'::jsonb
 
 select * from test where json ? 'friends' -> 2;
 
@@ -81,31 +169,46 @@ from test t, jsonb_array_elements(t.json -> 'friends') AS element
 where element ->> 'name' = 'Alan Mulligan';
 
 
-
 SELECT element -> 'offer_currencies' -> 0 -> 'value'
-FROM   test t, jsonb_array_elements(t.json -> 'matrix') AS element
+FROM   test t, 
+       jsonb_array_elements(t.json -> 'matrix') AS element
 WHERE  element ->> 'payment_selection' = 'mc'
 
 SELECT element -> 'offer_currencies' -> 0
-FROM   test t, jsonb_array_elements(t.json -> 'matrix') AS element
+FROM   test t, 
+       jsonb_array_elements(t.json -> 'matrix') AS element
 WHERE  element ->> 'payment_selection' = 'mc'
 
 SELECT element -> 'offer_currencies'
-FROM   test t, jsonb_array_elements(t.json -> 'matrix') AS element
+FROM   test t, 
+       jsonb_array_elements(t.json -> 'matrix') AS element
 WHERE  element ->> 'payment_selection' = 'mc'
 
 SELECT element
-FROM   test t, jsonb_array_elements(t.json -> 'matrix') AS element
+FROM   test t, 
+       jsonb_array_elements(t.json -> 'matrix') AS element
 WHERE  element ->> 'payment_selection' = 'mc'
 
-SELECT first_currency.*
+SELECT offer.*
 FROM   test t,
        jsonb_array_elements(t.json -> 'matrix') element,
-       jsonb_each_text(element -> 'offer_currencies' -> 0) AS first_currency
-WHERE  element ->> 'payment_selection' = 'mc'
- 
+       jsonb_each_text(element -> 'offer_currencies' -> 3) AS offer
+WHERE  element ->> 'payment_selection' = 'unselected'
 
-select '{"a":"abc","d":"def","z":[1,2,3],"d":"overwritten"}'::jsonb -> 'z' -> 2;
+SELECT length 
+FROM   test t,
+       jsonb_array_elements(t.json -> 'matrix') element,
+       jsonb_array_length(element -> 'offer_currencies') AS length
+WHERE  element ->> 'payment_selection' = 'vb'
+
+SELECT typeOf
+FROM   test t, 
+       jsonb_array_elements(t.json -> 'matrix') AS element,
+       jsonb_typeof(element -> 'offer_currencies' -> 0 -> 'currency_name') AS typeOf
+WHERE  element ->> 'payment_selection' = 'mc'
+
+SELECT id, json -> 'lookup_id'
+FROM   test
 
 create index idx on test using gin (json);
 drop index idx;
